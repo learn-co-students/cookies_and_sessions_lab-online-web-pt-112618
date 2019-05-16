225a659b121ce23f8126b4d15f528b4a49c95f8b
Michael Schiumo <mschiumo18@gmail.com>
2019-05-16 17:07:20 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,9 @@
-class ProductsController < Application ActionController
+class ProductsController < ApplicationController
 
   def index
     @products = Product.all
   end
 
   def add_to_cart
-  end  
+  end
 end
